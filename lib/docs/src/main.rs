05b36b64b72b8083dfb407d90f0440b400f606c5
delisa fuller <dfuller@bitdrift.io>
2026-07-08 17:30:26 +0100

    remove unused

--- lib/docs/src/main.rs
+++
@@ -1,8 +0,0 @@
-use std::process::ExitCode;
-
-use clap::Parser;
-use vrl::docs::{Opts, cmd::docs};
-
-fn main() -> ExitCode {
-    ExitCode::from(docs(&Opts::parse(), &vrl::stdlib::all()) as u8)
-}
